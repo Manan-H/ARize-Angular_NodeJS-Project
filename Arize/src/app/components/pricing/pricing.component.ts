77d2a8e4cea958bea0b7d0b884ca6549f81280b9
Manan Hambardzumyan <manan.hambardzumyan@gmail.com>
2019-03-12 15:40:36 +0400

    pricing modal added

--- Arize/src/app/components/pricing/pricing.component.ts
+++ Arize/src/app/components/pricing/pricing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pricing',
@@ -8,12 +9,25 @@ import { DataService } from '../../data.service';
 })
 export class PricingComponent implements OnInit {
   prices: any = [];
+  priceForm: FormGroup;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.getPricing().subscribe(data => {
       return (this.prices = data);
+    });
+
+    this.priceForm = new FormGroup({
+      first_name: new FormControl('', Validators.required),
+      user_email: new FormControl('', [Validators.required, Validators.email]),
+      quantity_2d: new FormControl('', Validators.required),
+      quantity_3d: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      captcha: new FormControl(
+        '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
+        Validators.required
+      )
     });
   }
 
@@ -27,5 +41,14 @@ export class PricingComponent implements OnInit {
       pricingBtn[i].classList.toggle('selected_btn');
       pricingDesc[i].classList.toggle('hideDiv');
     }
+  }
+
+  resolved(captchaResponse: string) {
+    console.log(`Resolved captcha with response ${captchaResponse}:`);
+  }
+
+  doPurchase() {
+    let answer = document.querySelector('#success_message');
+    answer.setAttribute('style', 'display: block;');
   }
 }
